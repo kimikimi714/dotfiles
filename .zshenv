3161655a68442df9bea055a9b7a19183db772447
kimikimi714 <kimikimi714@gmail.com>
2014-06-08 05:34:14 +0900

    zshの修正
    - gitしか使ってないので、gitの状態変化だけが分かるようにzshrc.customを変更
    - lsコマンドで色がつくようにglsへのalias追加
    - brewでcoreutil追加
    - PATHの設定はzshenvでするように変更

---
+++ .zshenv
@@ -0,0 +1,6 @@
+# coreutilsの設定
+export PATH="$(brew --prefix coreutils)/libexec/gnubin:$PATH"
+# rbenvの設定
+export PATH="$HOME/.rbenv/bin:$PATH"
+eval "$(rbenv init - zsh)"
+
---
+++ custom-aliases.zsh
@@ -0,0 +1,9 @@
+# 特定ディレクトリへのalias
+alias GC="cd ${DOCUMENTS_DIR}/git_clone"
+alias GR="cd ${DOCUMENTS_DIR}/git_repo"
+alias V="cd ${DOCUMENTS_DIR}/vagrant"
+
+# 実行時オプションとかショートカットaliasの設定
+alias ls='gls --color -F'
+alias pass='mono ~/Downloads/KeePass-2.26/KeePass.exe'
+
