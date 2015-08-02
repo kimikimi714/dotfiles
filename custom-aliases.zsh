3b8d7edc25c1593010223c92ed7f599fae7c6a1c
kimikimi714 <kimikimi714@gmail.com>
2015-08-02 20:33:28 +0900

    外付けへのリンク貼った

--- custom-aliases.zsh
+++ custom-aliases.zsh
@@ -1,9 +1,9 @@
 # 特定ディレクトリへのalias
-alias GC="cd ${DOCUMENTS}/git_clone"
-alias GR="cd ${DOCUMENTS}/git_repo"
+alias GC="cd ${MOUNT_DISK}/git_clone"
+alias GR="cd ${MOUNT_DISK}/git_repo"
 alias V="cd ${DOCUMENTS}/vagrant"
 
 # 実行時オプションとかショートカットaliasの設定
 alias ls='gls --color -F'
-alias pass='mono ~/Downloads/KeePass-2.26/KeePass.exe'
-
+alias pass="mono ${MOUNT_DISK}/downloads/KeePass-2.26/KeePass.exe"
+alias uml="java -jar ${MOUNT_DISK}/downloads/jars/plantuml.jar"
